90a58c2400ac84ae844a9788c4271b7080bad434
Pranav Ramesh <laphatize.me@gmail.com>
2018-08-18 23:05:59 -0400

    Updated source code.
    Updates :o

--- coin.py
+++ coin.py
@@ -1,39 +1,67 @@
 # pgzrun /Users/Ramesh/Desktop/Pranav/Python-Projects/coin-collector/coin.py 
-# laphatize
 
 
 from random import randint
+
+
+DEVELOPERMODE = True #  Set this to True to see important details logged to console.
+
 
 WIDTH = 400 # do not change, sets size of windows
 
 HEIGHT = 400 # ^
 
+
 score = 0 # < change that if ya want, don't see why. 
 
 game_over = False # prevents the gameover screen from showing at start
 
-# fox image, and initial position
+# Fox image, and initial position
 fox = Actor("fox")
 fox.pos = 100, 100
 
-# get coin image, and initial position
+# Get coin image, and initial position
 coin = Actor("coin") 
 coin.pos = 200, 200
+ 
+# TO-DO
+# spike = Actor("spike")
+# spike.pos = 200, 200
+#
+#
+# def place_spike():
+#	spike.x = randint(20, (WIDTH - 20))
+#	spike.y = randint(20, (HEIGHT - 20))
+
+
+
+#	if DEVELOPERMODE:
+#		print(spike.x)
+#		print(spike.y)
+
 
 def draw():
 
-	# the screen you see when you play
-	screen.fill("lightgreen") # background (Grass)
-	fox.draw() # the fox, thanks random person who made it
-	coin.draw() # the coin
+	# Set's main gamespace
+	screen.fill("lightgreen") 
+	fox.draw() 
+	coin.draw()
+#	spike.draw()
 	screen.draw.text("Score: " + str(score), color="black", topleft=(10,10))
 	screen.draw.text("Laphatize Creations", color="black", topleft=(10,30))
 
-	# game over screen, rip
+	# Game Over Screeb
 	if game_over:
-		screen.fill("pink")
-		screen.draw.text("Times Up! :( ", topleft=(10,60), fontsize=60)
-		screen.draw.text("Final Score: " + str(score), color="black", topleft=(10,10), fontsize=60)
+		
+		screen.fill("pink") # Change background to pink.
+
+		screen.draw.text("Times Up! :( ", topleft=(10,60), fontsize=60) 
+
+		screen.draw.text("Final Score: " + str(score), color="black", topleft=(10,10), fontsize=60) 
+		
+		screen.draw.text("R.I.P Fox 2018.", topleft=(10,1))
+
+
 
 def place_coin():
 	coin.x = randint(20, (WIDTH - 20))
@@ -44,32 +72,47 @@ def time_up():
 	game_over = True
 	place_coin()
 
+
+
+
+
 def update():
 	global score
 
+	speed = 5
+
 
 	if keyboard.left:
-		fox.x = fox.x - 2
+		fox.x = fox.x - speed
+		fox.image = "foxleft"
 
 	if keyboard.right:
-		fox.x = fox.x + 2
-
+		fox.x = fox.x + speed
+		fox.image = "fox"
 
 
 	if keyboard.up:
-		fox.y = fox.y - 2
+		fox.y = fox.y - speed
 
 	if keyboard.down:
-		fox.y = fox.y + 2
+		fox.y = fox.y + speed
 
 	coin_collected = fox.colliderect(coin)
 
 	if coin_collected:
 		score = score + 10
 		place_coin()
+		sounds.piccoin.play()
+#		place_spike()
+		speed = speed - .2
+
+#	spike_touched = fox.colliderect(spike)
+
+#	if spike_touched:
+#		game_over = True
 
 
 
 
-clock.schedule(time_up, 7.0)
+clock.schedule(time_up, 16.0)
 place_coin()
